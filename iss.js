5119a208c66bb85a3eb58017ee9d658da6a923ca
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-13 17:15:18 -0500

    added fetchCoordByID fn

--- iss.js
+++ iss.js
@@ -16,4 +16,25 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body).data;
+    
+    //const result = { latitude: data.data.latitude, longitude: data.data.longitude };
+    callback(null, { latitude, longitude });
+  });
+}
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
+
